fb3cf76ddf49b839041064440776000e5b6f017b
root <root@DESKTOP-MU6DMP3.localdomain>
2018-09-02 16:03:10 +0900

    update line feed setting

--- src/window.css.d.ts
+++ src/window.css.d.ts
@@ -1,13 +1,13 @@
 declare namespace window {
-	const windowWrapper: string
-	const top: string
-	const left: string
-	const right: string
-	const center: string
-	const bottom: string
-	const window: string
-	const titlebar: string
-	const resize: string
-	const close: string
+  const windowWrapper: string;
+  const top: string;
+  const left: string;
+  const right: string;
+  const center: string;
+  const bottom: string;
+  const window: string;
+  const titlebar: string;
+  const resize: string;
+  const close: string;
 }
-export default window
+export default window;
